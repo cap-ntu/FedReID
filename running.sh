84ab0a9f5d0262e62a7d2ef17b64b6f017a112ee
gan xin <ganxin@sensetime.com>
2021-01-01 22:33:24 +0800

    base

--- running.sh
+++ running.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:$pwd
-srun -u --partition=Sensetime --job-name=fed_clustering \
+srun -u --partition=Sensetime --job-name=fed_base \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
     python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all | tee fed_reid_base.log &
