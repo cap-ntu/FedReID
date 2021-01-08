e9bfdced1f1588c28c37b0154931f83e7de5983b
gan xin <ganxin@sensetime.com>
2021-01-08 11:04:58 +0800

    cdw 41

--- running_clustering_cdw.sh
+++ running_clustering_cdw.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:$pwd
 srun -u --partition=Sensetime --job-name=clu_cdw \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --cdw | tee fed_reid_clustering_cdw_cpk.log &
+    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --cdw --resume_epoch 41 | tee fed_reid_clustering_cdw_cpk_from_41.log &
