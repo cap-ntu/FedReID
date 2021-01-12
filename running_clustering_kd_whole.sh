4359813a274447dba012b6dd6af931bf95fa08c1
gan xin <ganxin@sensetime.com>
2021-01-12 23:41:09 +0800

    cdw kd

--- running_clustering_kd_whole.sh
+++ running_clustering_kd_whole.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:$pwd
-srun -u --partition=Sensetime --job-name=clu \
+srun -u --partition=Sensetime --job-name=clu_kd_whole \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --kd --regularization --kd_method whole | tee fed_reid_clustering_cpk.log &
+    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --kd --regularization --kd_method whole | tee fed_reid_clustering_kd_whole.log &
