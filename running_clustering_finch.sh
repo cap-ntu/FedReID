06fcc540846ecce376ce4e10d372967cfbe5e9f7
gan xin <ganxin@sensetime.com>
2021-02-02 18:27:42 +0800

    sh file

--- running_clustering_finch.sh
+++ running_clustering_finch.sh
@@ -1,4 +1,5 @@
+▽
 export PYTHONPATH=$PYTHONPATH:$pwd
-srun -u --partition=Sensetime --job-name=clu_finch_0.9 \
+srun -u --partition=innova --job-name=f0.9 \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --clustering_method finch --max_distance 0.9 | tee fed_reid_clustering_cpk_finch_0.9.log &
+    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --clustering_method finch --max_distance 0.9 --resume_epoch 119 | tee fed_reid_clustering_cpk_finch_0.9_from_119.log &
--- running_clustering_kmeans.sh
+++ running_clustering_kmeans.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:$pwd
-srun -u --partition=Sensetime --job-name=clu_kmeans_2 \
+srun -u --partition=innova --job-name=kmeans_2 \
     -n1 --gres=gpu:1 --ntasks-per-node=1 \
-    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --clustering_method kmeans --n_cluster 2 | tee fed_reid_clustering_cpk_kmeans_2.log &
+    python  main.py --data_dir /mnt/lustre/ganxin/fedreid_data/data --train_all --clustering --clustering_method kmeans --n_cluster 2 --resume_epoch 149 | tee fed_reid_clustering_cpk_kmeans_2_from_149.log &
