5358a85e708017e7bfdcb9f1ea08b6f1336272b5
codergan <ganx0005@e.ntu.edu.sg>
2021-05-05 11:45:39 +0800

    testing on remove load and store mat, test each round

--- server.py
+++ server.py
@@ -213,17 +213,17 @@ class Server():
                 'query_cam': self.data.query_meta[dataset]['cameras']
             }
             print("====== before loading =======")
-            for i in result:
-                print(i, np.array(result[i]).shape, result[i][:3])
-            file_path = os.path.join(self.project_dir,
-                                     'model',
-                                     self.model_name,
-                                     'pytorch_result_{}_{}.mat'.format(dataset, random.randint(0, 100000000)))
-            scipy.io.savemat(file_path, result)
+            # for i in result:
+            #     print(i, np.array(result[i]).shape, result[i][:3])
+            # file_path = os.path.join(self.project_dir,
+            #                          'model',
+            #                          self.model_name,
+            #                          'pytorch_result_{}_{}.mat'.format(dataset, random.randint(0, 100000000)))
+            # scipy.io.savemat(file_path, result)
 
             print(self.model_name)
             print(dataset)
-            testing_model(file_path, dataset)
+            testing_model(result, dataset)
             # os.system('python evaluate.py --result_dir {} --dataset {}'.format(os.path.join(self.project_dir, 'model', self.model_name), dataset))
 
     def knowledge_distillation(self, regularization, kd_method):
